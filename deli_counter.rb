dcf32e8aa99f52d2156a6891b3f14bdc9026e5ef
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-02-22 10:31:39 -0800

    commit tree e2c6d9a2a4c3ba3c2ca55470343a5584e178bd6c

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def line array
+    if array.length == 0
+        puts "The line is currently empty."
+    else
+        new_arr = array.map do |name|
+            "#{array.index(name) + 1}. #{name}"
+        end
+        puts "The line is currently: #{new_arr.join(" ")}"
+    end
+end
+
+def take_a_number array, new_name
+    array << new_name
+    puts "Welcome, #{new_name}. You are number #{array.index(new_name) + 1} in line."
+end
+
+def now_serving array
+    if array.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array[0]}."
+        array.shift
+    end
+end
